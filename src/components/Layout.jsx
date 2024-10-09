7ea87a09928d83182e23a7ce73fd24b778f7003e
Maheep Gupta <maheepgupta321@gmail.com>
2024-10-10 00:16:13 +0530

    reverted a commit 012d0f1

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -9,29 +9,21 @@ import Events from './Events';
 const Layout = () => {
     return (
         <>
-<<<<<<< HEAD
-            <header className='xl:p-5 lg:p-2 flex items-center border-2 bg-white'>
+            <header className='p-5 flex items-center border-2 bg-white'>
                 <Navbar />
             </header>
-            <main className='grid xl:grid-cols-[minmax(0,1fr)_minmax(0,3fr)_minmax(0,1fr)] lg:grid-cols-[minmax(0,1fr)_minmax(0,3fr)_minmax(0,1fr)] xl:p-5 justify-between relative lg:p-2'>
-                <aside className='xl:m-3 lg:m-1'>
+            <main className=' main-grid p-5 justify-between relative'>
+                <aside className='m-3'>
                     <div className="sticky top-5">
 
-=======
-       <header className='p-5 flex items-center border-2 bg-white'>
-                <Navbar />
-            </header>
-            <main className=' flex p-5 justify-between'>
-                <aside className='m-3'>
->>>>>>> parent of 5f8fd94 (feed section completed)
                     <UserInfo />
-                    <Navigation/>
+                    <Navigation />
+                    </div>
                 </aside>
                 <section>
-                    <Outlet/>
+                    <Outlet />
                 </section>
-<<<<<<< HEAD
-                <aside className='xl:m-3 lg:m-1 relative '>
+                <aside className='m-3 relative '>
                     <div className='sticky top-5 overflow-scroll hide-scrollbar h-screen'>
                         <div className="scroll">
 
@@ -39,11 +31,6 @@ const Layout = () => {
                         <Events />
                         </div>
                     </div>
-=======
-                <aside className='m-3'>
-                    <Messages />
-                    <Events/>
->>>>>>> parent of 5f8fd94 (feed section completed)
                 </aside>
             </main>
         </>
--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -9,40 +9,39 @@ import { NavLink } from 'react-router-dom'
 const Messages = () => {
     return (
         <>
-<<<<<<< HEAD
             <div className='bg-white p-3 rounded-xl text-sm border-2'>
-=======
-            <div className='bg-white p-3 w-[255px] rounded-xl text-sm'>
->>>>>>> parent of 5f8fd94 (feed section completed)
-                <div className='flex justify-between pb-4'>
 
-                    <div className='font-semibold font-pop text-[16px]'>Messages</div>
-                    <FiEdit />
-                </div>
-                <div className='flex bg-bgPink mb-2 font-robo items-center rounded-lg xl:h-10 lg:h-8 gap-2 px-2'>
-                    <span className='h-4 w-4 '>
-                        <svg className='h-4 w-4' xmlns="http://www.w3.org/2000/svg" fill='#808080' viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" /></svg>
-                    </span>
-                    <span className='bg-inherit w-full'>
-                        <input type="text" placeholder='Search' className='bg-inherit w-full outline-none' />
-                    </span>
-                    <IoFilter className='h-5 w-5 text-[#808080]'/>
-                </div>
-                <div className="msg-nav flex justify-between border-b-2 items-center ">
-                    <div className='flex justify-between lg:text-xs'>
-                        <span className='capitalize font-semibold p-2 border-b-2 border-black cursor-pointer'>primary</span>
-                        <span className='capitalize p-2 cursor-pointer'>General</span>
+                <div className='bg-white p-3 w-[255px] rounded-xl text-sm'>
+                    <div className='flex justify-between pb-4'>
+
+                        <div className='font-semibold font-pop text-[16px]'>Messages</div>
+                        <FiEdit />
                     </div>
-                    <div>
-                        <span className='capitalize p-2 text-blue-600 cursor-pointer'>requests(4)</span>
+                    <div className='flex bg-bgPink mb-2 font-robo items-center rounded-lg xl:h-10 lg:h-8 gap-2 px-2'>
+                        <span className='h-4 w-4 '>
+                            <svg className='h-4 w-4' xmlns="http://www.w3.org/2000/svg" fill='#808080' viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" /></svg>
+                        </span>
+                        <span className='bg-inherit w-full'>
+                            <input type="text" placeholder='Search' className='bg-inherit w-full outline-none' />
+                        </span>
+                        <IoFilter className='h-5 w-5 text-[#808080]' />
                     </div>
+                    <div className="msg-nav flex justify-between border-b-2 items-center ">
+                        <div className='flex justify-between lg:text-xs'>
+                            <span className='capitalize font-semibold p-2 border-b-2 border-black cursor-pointer'>primary</span>
+                            <span className='capitalize p-2 cursor-pointer'>General</span>
+                        </div>
+                        <div>
+                            <span className='capitalize p-2 text-blue-600 cursor-pointer'>requests(4)</span>
+                        </div>
+                    </div>
+                    <div className='py-4 '>
+                        {friends.map((ele) => <Friend friendName={ele.friendsName} avatar={avatar} onlineStatus={true} />)}
+                    </div>
+                    <NavLink className="pl-3 font-pop text-gray-400 text-xs cursor-pointer" to="/friends">
+                        View All
+                    </NavLink>
                 </div>
-                <div className='py-4 '>
-                    {friends.map((ele) => <Friend friendName={ele.friendsName} avatar={avatar} onlineStatus={true}/>)}
-                </div>
-                <NavLink className="pl-3 font-pop text-gray-400 text-xs cursor-pointer" to="/friends">
-                View All
-                </NavLink>
             </div>
         </>
     )
