8ca08605aa2c8120677ed32441c940c2ce5d2754
Maheep Gupta <maheepgupta321@gmail.com>
2025-04-06 00:27:27 +0530

    chnages in route logic and screen changes

--- src/components/chat/ChatSection.jsx
+++ src/components/chat/ChatSection.jsx
@@ -1,32 +1,28 @@
 import React from 'react'
-import ChattingScreen from './ChattingScreen'
 import { IoArrowBack } from 'react-icons/io5'
 import Friend from '../Friend'
 import friends from '../../utils/friends'
 import avatar from '../../assets/avatarforFriends.avif'
+import { Link, Outlet, useNavigation } from 'react-router-dom'
+
 
 const ChatSection = () => {
     return (
         <main className=' '>
             <div class="flex h-screen antialiased text-gray-800">
                 <div class="flex flex-row h-full w-full overflow-x-hidden overflow-hidden">
-                    <div class="flex flex-col py-8 pl-6 pr-2 w-64 bg-white flex-shrink-0">
-                        <div class="flex flex-row items-center justify- h-12 w-full">
-                            <div
+                    <div class="flex flex-col py-8 pl-6 pr-2 w-64 bg-white flex-shrink-0 border-r shadow-2xl">
+
+                        <div class="flex flex-row items-center justify- h-12 w-full ">
+                            <Link to='/'
                                 class="flex items-center justify-center rounded-2xl text-indigo-700 bg-indigo-100 h-10 w-10"
-                            onClick={() => window.history.back()}
+                                
                             >
                                 <IoArrowBack size={22} className='h-8' />
-                            </div>
+                            </Link>
                             <div class="ml-4 font-bold text-2xl ">Messages</div>
                         </div>
-
-
-                        {/* <UserCard /> */}
-
-
-
-                        <div class="flex flex-col mt-8">
+                        <div class="flex-1 flex-col mt-8">
                             <div class="flex flex-row items-center justify-between text-xs">
                                 <span class="font-bold">Your Conversations</span>
                                 <span
@@ -39,7 +35,12 @@ const ChatSection = () => {
                             <div class="flex flex-col space-y-1 mt-4 -mx-2 h-4/5 overflow-y-auto">
 
                                 <div className='py-4'>
-                                    {friends.map((ele) => <Friend friendName={ele.friendsName} avatar={avatar} onlineStatus={true} />)}
+                                    {friends.map((ele,i) =>
+                                        <Link to={`/friends/chat/${i}`} className='py-1 border-b hover:bg-gray-200 hover:shadow-xl flex justify-between items-center pr-2'>
+                                            <Friend friendName={ele.friendsName} avatar={avatar} onlineStatus={true} />
+                                        </Link>
+                                    )}
+
                                 </div>
                             </div>
 
@@ -47,7 +48,7 @@ const ChatSection = () => {
 
                         </div>
                     </div>
-                    <ChattingScreen />
+                    <Outlet />
                 </div>
             </div>
         </main>
