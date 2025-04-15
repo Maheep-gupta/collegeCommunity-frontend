d96cc69c99a6505932055ff1e736ed886c673ac6
Maheep Gupta <maheepgupta321@gmail.com>
2025-04-15 23:02:15 +0530

    ux fixtures in chatwithai

--- src/components/C-AI/ChatWithAI.jsx
+++ src/components/C-AI/ChatWithAI.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import MessageAi from "./MessageAi";
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import { FaBookOpen, FaCompass, FaHistory, FaHome } from 'react-icons/fa';
+import { IoArrowBack } from 'react-icons/io5';
 
 function ChatWithAI() {
 
@@ -10,22 +11,30 @@ function ChatWithAI() {
       {/* Sidebar */}
       <aside className="w-64 p-4 h-full text-black border-r border-gray-300 shadow-sm flex flex-col justify-between">
         <div>
-          <h1 className="text-2xl font-bold mb-6">ChatWithAI</h1>
-          <nav className="space-y-4">
-            <div className="flex items-center gap-2 p-2 hover:shadow-lg hover:rounded-md hover:scale-105 text-black cursor-pointer">
+          <div class="flex flex-row items-center mb-5 justify- h-12 w-full ">
+            <Link to='/'
+              class="flex items-center justify-center rounded-2xl text-indigo-700 bg-indigo-100 h-10 w-10"
+
+            >
+              <IoArrowBack size={22} className='h-8' />
+            </Link>
+            <div class="ml-4 font-bold text-2xl ">ChatWithAI</div>
+          </div>
+          <nav className="space-y-5">
+            <div className="flex items-center gap-2 p-2 hover:bg-gray-200 h-12 hover:shadow-lg hover:rounded-md hover:scale-105 text-black cursor-pointer">
               <FaHome size={25} />
               <span className='text-lg'>Home</span>
             </div>
-            <div className="flex items-center gap-2 p-2 hover:shadow-lg hover:rounded-md hover:scale-105 text-black cursor-pointer">
+            <div className="flex items-center gap-2 p-2 hover:bg-gray-200 h-12 hover:shadow-lg hover:rounded-md hover:scale-105 text-black cursor-pointer">
               <FaCompass size={25} />
               <span className='text-lg'>Discover</span>
             </div>
-            <div className="flex items-center gap-2 p-2 hover:shadow-lg hover:rounded-md hover:scale-105 text-black cursor-pointer">
+            <div className="flex items-center gap-2 p-2 hover:bg-gray-200 h-12 hover:shadow-lg hover:rounded-md hover:scale-105 text-black cursor-pointer">
 
               <FaBookOpen size={25} />
               <span className='text-lg'>Library</span>
             </div>
-            <div className="flex items-center gap-2 p-2 hover:shadow-lg hover:rounded-md hover:scale-105 text-black cursor-pointer">
+            <div className="flex items-center gap-2 p-2 hover:bg-gray-200 h-12 hover:shadow-lg hover:rounded-md hover:scale-105 text-black cursor-pointer">
 
               <FaHistory size={25} />
               <span className='text-lg'>History</span>
