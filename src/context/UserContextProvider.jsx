505600f763ff1382de56f737ab16134281bb8b06
Maheep Gupta <maheepgupta321@gmail.com>
2025-04-17 02:46:48 +0530

    user login context done

---
+++ src/context/UserContextProvider.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import userContext from './UserContext'
+
+function UserContextProvider({ children }) {
+    const [user, setUser] = useState(null)
+    const [isAdmin, setIsAdmin] = useState(false)
+
+    const login = (userData) => {
+        setUser(userData);
+        //  store token in localStorage
+        localStorage.setItem('user', JSON.stringify(userData));
+
+        // logic to set is admin based on userData
+    };
+    
+
+    const logout = () => {
+        setUser(null);
+        localStorage.removeItem('user');
+        setIsAdmin(false);
+    };
+    return (
+        <userContext.Provider value={{ user, login, logout }}>
+            {children}
+        </userContext.Provider>
+    )
+}
+
+export default UserContextProvider
